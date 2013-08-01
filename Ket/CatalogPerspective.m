fe326e6a7573b4b68c51c7350525c68dc0cf2e4a
Tomoki Aonuma <uasi@uasi.jp>
2013-08-01 14:04:36 +0900

    CatalogPerspectiveFiltered (some sort of)

--- Ket/CatalogPerspective.m
+++ Ket/CatalogPerspective.m
@@ -20,9 +20,14 @@
 @property (nonatomic, readonly) NSUInteger numberOfCirclesInCollection;
 @property (nonatomic, readonly) NSString *viewName;
 
+// XXX: for CirclePerspectiveFiletered
+- (instancetype)initWithDatabase:(CatalogDatabase *)database;
+- (void)dropView;
+
 @end
 
-#pragma mark - CatalogPerspectiveDefault (Concrete Subclass)
+#pragma mark -
+#pragma mark CatalogPerspectiveDefault (Concrete Subclass)
 
 @interface CatalogPerspectiveDefault : CatalogPerspective
 
@@ -30,19 +35,17 @@
 
 @implementation CatalogPerspectiveDefault
 
+- (CircleCollection *)circleCollectionForPage:(NSUInteger)page
+{
+  NSArray *circles = [self.database circlesInPage:page];
+  return [[CircleCollection alloc] initWithCircles:circles cutCountPerPage:self.numberOfCirclesInCollection respectsCutIndex:YES];
+}
+
+#pragma mark Concrete Methods
+
 - (NSUInteger)numberOfCircleCollections
 {
   return self.database.pageSet.count;
-  // XXX: will be used by a filtered perspective
-  /*
-   BOOL hasRemainder = self.numberOfCircles % self.numberOfCirclesInCollection != 0;
-   return self.numberOfCircles / self.numberOfCirclesInCollection + (hasRemainder ? 1 : 0);
-   */
-}
-
-- (NSUInteger)numberOfCirclesInCollection
-{
-  return self.database.numberOfCutsInRow * self.database.numberOfCutsInColumn;
 }
 
 - (CircleCollection *)circleCollectionAtIndex:(NSUInteger)index
@@ -52,33 +55,64 @@
   return [self circleCollectionForPage:page];
 }
 
-- (NSUInteger)pageAtIndex:(NSUInteger)index
+@end
+
+#pragma mark -
+#pragma mark CatalogPerspectiveFiltered (Concrete Subclass)
+
+@interface CatalogPerspectiveFiltered : CatalogPerspective
+
+@end
+
+@implementation CatalogPerspectiveFiltered
+
+- (instancetype)initWithDatabase:(CatalogDatabase *)database
 {
-  __block NSUInteger page = 0;
-  __block NSUInteger i = index;
+  self = [super initWithDatabase:database];
 
-  [self.database.pageSet enumerateRangesUsingBlock:^(NSRange range, BOOL *stop) {
-    if (i > range.length) {
-      i -= range.length + 1;
-    }
-    else {
-      page = range.location + i;
-      *stop = YES;
-    }
-  }];
+  // XXX: a simple and stupid filter
+  [self dropView];
+  static NSString *sqlFormat = (@"CREATE TEMPORARY VIEW %@"
+                                @"  AS SELECT * FROM ComiketCircle WHERE pageNo < 100 AND pageNo != 0;");
+  NSString *sql = [NSString stringWithFormat:sqlFormat, self.viewName];
+  NSAssert([self.fmDatabase executeUpdate:sql], @"CREATE VIEW must succeed");
 
-  return page;
+  return self;
 }
 
-- (CircleCollection *)circleCollectionForPage:(NSUInteger)page
+- (NSArray *)circlesWithLimit:(NSUInteger)limit offset:(NSUInteger)offset
 {
-  NSArray *circles = [self.database circlesInPage:page];
-  return [[CircleCollection alloc] initWithCircles:circles cutCountPerPage:self.numberOfCirclesInCollection respectsCutIndex:YES];
+  static NSString *sqlFormat = @"SELECT * FROM %@ LIMIT %lu OFFSET %lu;";
+  NSString *sql = [NSString stringWithFormat:sqlFormat, self.viewName, (unsigned long)limit, (unsigned long)offset];
+  FMResultSet *result = [self.fmDatabase executeQuery:sql, self.viewName];
+  NSMutableArray *circles = [NSMutableArray array];
+  while ([result next]) {
+    [circles addObject:[Circle circleWithResultSet:result]];
+  }
+  return [circles copy];
+}
+
+#pragma mark Concrete Methods
+
+- (NSUInteger)numberOfCircleCollections
+{
+   BOOL hasRemainder = self.numberOfCircles % self.numberOfCirclesInCollection != 0;
+   return self.numberOfCircles / self.numberOfCirclesInCollection + (hasRemainder ? 1 : 0);
+}
+
+- (CircleCollection *)circleCollectionAtIndex:(NSUInteger)index
+{
+  NSAssert(index < self.numberOfCircleCollections, @"index must be less than the number of circle collections");
+  NSUInteger limit = self.numberOfCirclesInCollection;
+  NSUInteger offset = index * self.numberOfCirclesInCollection;
+  NSArray *circles = [self circlesWithLimit:limit offset:offset];
+  return [[CircleCollection alloc] initWithCircles:circles cutCountPerPage:self.numberOfCirclesInCollection respectsCutIndex:NO];
 }
 
 @end
 
-#pragma mark - CatalogPerspective (Abstract Superclass)
+#pragma mark -
+#pragma mark CatalogPerspective (Abstract Superclass)
 
 @implementation CatalogPerspective
 
@@ -87,6 +121,8 @@
 + (CatalogPerspective *)perspectiveWithDatabase:(CatalogDatabase *)database
 {
   return [[CatalogPerspectiveDefault alloc] initWithDatabase:database];
+  // XXX: use this if you want to test CatalogPerspectiveFiltered
+  //return [[CatalogPerspectiveFiltered alloc] initWithDatabase:database];
 }
 
 - (instancetype)initWithDatabase:(CatalogDatabase *)database
@@ -123,6 +159,29 @@
   return self.count;
 }
 
+- (NSUInteger)numberOfCirclesInCollection
+{
+  return self.database.numberOfCutsInRow * self.database.numberOfCutsInColumn;
+}
+
+- (NSUInteger)pageAtIndex:(NSUInteger)index
+{
+  __block NSUInteger page = 0;
+  __block NSUInteger i = index;
+
+  [self.database.pageSet enumerateRangesUsingBlock:^(NSRange range, BOOL *stop) {
+    if (i > range.length) {
+      i -= range.length + 1;
+    }
+    else {
+      page = range.location + i;
+      *stop = YES;
+    }
+  }];
+
+  return page;
+}
+
 #pragma mark View Management
 
 - (void)createView
@@ -153,37 +212,10 @@
   return 0;
 }
 
-- (NSUInteger)numberOfCirclesInCollection
-{
-  NSAssert(NO, @"");
-  return 0;
-}
-
-// XXX: will be used by a filtered perspective
-/*
- - (NSArray *)circlesWithLimit:(NSUInteger)limit offset:(NSUInteger)offset
- {
- static NSString *sqlFormat = @"SELECT * FROM %@ LIMIT (?) OFFSET (?);";
- NSString *sql = [NSString stringWithFormat:sqlFormat, self.viewName];
- FMResultSet *result = [self.fmDatabase executeQuery:sql, self.viewName, @(limit), @(offset)];
- NSMutableArray *circles = [NSMutableArray array];
- while ([result next]) {
- [circles addObject:[Circle circleWithResultSet:result]];
- }
- return [circles copy];
- }
- */
-
 - (CircleCollection *)circleCollectionAtIndex:(NSUInteger)index
 {
   NSAssert(NO, @"");
   return nil;
-}
-
-- (NSUInteger)pageAtIndex:(NSUInteger)index
-{
-  NSAssert(NO, @"");
-  return 0;
 }
 
 @end

802e69c0b2c882882d462c8004c7399faf901904
Tomoki Aonuma <uasi@uasi.jp>
2013-08-14 19:21:28 +0900

    Typo

--- Ket/CatalogDatabase.h
+++ Ket/CatalogDatabase.h
@@ -5,7 +5,7 @@
 
 @property (nonatomic, readonly) NSInteger comiketNo;
 @property (nonatomic, readonly) NSSize cutSize DO_NOT_USE("cutSize is for printing purposes, which Ket doesn't support");
-@property (nonatomic, readonly) NSPoint cutOrigin DO_NOT_USE("cutOrigin is for printing purposes, which Ket dosn't support");
+@property (nonatomic, readonly) NSPoint cutOrigin DO_NOT_USE("cutOrigin is for printing purposes, which Ket doesn't support");
 @property (nonatomic, readonly) NSUInteger numberOfCutsInRow;
 @property (nonatomic, readonly) NSUInteger numberOfCutsInColumn;
 @property (nonatomic, readonly) NSIndexSet *pageSet;
